fd02b9b36740863a4595797d3d19cdd26e5a846c
Nicholas Bay <70332449+Nicholas-Bay@users.noreply.github.com>
2022-01-25 21:07:33 +0800

    updated header and footer js file

--- assets/headerNfooter.js
+++ assets/headerNfooter.js
@@ -4,7 +4,7 @@ class MyHeader extends HTMLElement {
         <style>
             li {margin-left: 20px; margin-right: 20px; }
         </style>
-        <navbar class="navbar navbar-expand-lg bg-dark navbar-dark">
+        <navbar class="navbar navbar-expand-lg bg-dark navbar-dark fixed-top">
             <div class="container">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navmenu">
                     <span class='navbar-toggler-icon'></span>
