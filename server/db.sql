c8dbf222e68de00976ff2c420ba148e51258c412
Nicholas Bay <70332449+Nicholas-Bay@users.noreply.github.com>
2022-02-04 23:41:40 +0800

    added db.sql

---
+++ server/db.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS shopee;
+CREATE DATABASE shopee;
+USE shopee;
+CREATE TABLE users (
+  username VARCHAR(50) PRIMARY KEY,
+  password VARCHAR(50) NOT NULL,
+  type VARCHAR(10) NOT NULL);
+CREATE TABLE products (
+  name VARCHAR(50) PRIMARY KEY,
+  description TEXT NOT NULL,
+  price FLOAT NOT NULL,
+  count SMALLINT NOT NULL,
+  image BLOB);
+INSERT INTO users VALUES
+  ('buyer', '123', 'buyer'),
+  ('admin', '123', 'seller');
