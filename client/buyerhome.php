8e918439fd5e146dbbbcd63936bc98d82a958d83
Nicholas Bay <70332449+Nicholas-Bay@users.noreply.github.com>
2022-02-06 17:32:15 +0800

    fixed cardview

--- client/buyerhome.php
+++ client/buyerhome.php
@@ -16,50 +16,48 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" type="text/css" href="style.css">
   <style>
-  .card {
-    transition: transform .2s; /* Animation */
-  }
-
-  .card:hover {
-    transform: scale(1.5); /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
-  }
+    .card {
+      transition: transform .2s;
+    }
+    .card:hover {
+      transform: scale(1.25);
+    }
   </style>
 </head>
 
 <body class='pt-5 pb-5'>
   <?php include 'header-buyer.php' ?>
-  <!-- CardView to Display Products -->
+  <!-- display products -->
   <section class='bg-dark text-light p-5 text-right text-sm-start'>
     <div class="container">
-      <div class="row">
-        <div class="d-flex justify-content-center align-items-center">
-          <?php
-          include '../server/config.php';
-          $conn = new mysqli($host, $user, $password, $db);
-          $sql = "SELECT * FROM $tableproduct";
-          $result = $conn->query($sql);
-          if (!empty($result) && $result->num_rows > 0) {
-            $count = 1;
-            echo "<div class=\"col\"><div class=\"row\">";
-            while ($row = $result->fetch_assoc()) {
-              echo "<div class=\"card\" style=\"width: 18rem; margin: 30px;\">" .
-              "<img class=\"card-img-top\" src=\"...\" alt=\"Card image cap\">" .
-              "<div class=\"card-body\">" .
-              "<h5 class=\"card-title text-dark\">" . $row['name'] . " ($" . $row['price'] . ")</h5>" .
-              "<h5 class=\"card-text\">" . $row['description'] . "</h5>" .
-              "<h5 class=\"card-text\">Remaining stock: " . $row['count'] . "</h5>" .
-              "<a href=\"#\" class=\"btn btn-primary\">Add to cart</a>" .
-              "</div></div>";
-              
-              if($count % 3 == 0) {
-                echo "</div><div class=\"row\">";
-              }
-              $count++;
-            }
-            echo "</div>";
+      <div class="d-flex justify-content-center align-items-center">
+        <?php
+        include '../server/config.php';
+        $conn = new mysqli($host, $user, $password, $db);
+        $sql = "SELECT * FROM $tableproduct";
+        $result = $conn->query($sql);
+        if (!empty($result) && $result->num_rows > 0) {
+          echo "<div class='col'><div class='row'>";
+          for ($count = 1; $row = $result->fetch_assoc(); $count++) {
+            if ($count == 1 || ($count - 1) % 4 == 0) echo "<div class='row'>";
+            echo "
+              <div class='col-sm-3'>
+                <div class='card' style='width: 18rem; margin: 30px;'>
+                  <img class='card-img-top' src=" . $row['image'] . " alt='image missing'>
+                  <div class='card-body'>
+                    <h5 class='card-title text-dark'>" . $row['name'] . " ($" . $row['price'] . ")</h5>
+                    <h5 class='card-text'>" . $row['description'] . "</h5>
+                    <h5 class='card-text'>Remaining stock: " . $row['count'] . " Left</h5>
+                    <a href='#' class='btn btn-warning pull-right'>Add to cart</a>
+                  </div>
+                </div>
+              </div>
+            ";
+            if($count % 4 == 0) echo "</div>";
           }
-          ?>
-        </div>
+        }
+        ?>
+      </div>
       </div>
     </div>
   </section>
