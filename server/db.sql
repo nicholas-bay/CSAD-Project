4d73d5e716422dc8008c4f241f26404d3f7353b5
Eugene3129 <eugene.quek03@gmail.com>
2022-02-08 13:36:34 +0800

    DB Update

--- server/db.sql
+++ server/db.sql
@@ -4,13 +4,20 @@ USE shopee;
 CREATE TABLE users (
   username VARCHAR(50) PRIMARY KEY,
   password VARCHAR(50) NOT NULL,
-  type VARCHAR(10) NOT NULL);
+  type VARCHAR(10) NOT NULL)
+  ENGINE = InnoDB;
 CREATE TABLE products (
   name VARCHAR(50) PRIMARY KEY,
   description TEXT NOT NULL,
   price FLOAT NOT NULL,
   count SMALLINT NOT NULL,
-  image LONGBLOB NOT NULL);
+  image LONGBLOB NOT NULL)
+  ENGINE = InnoDB;
+CREATE TABLE cart ( 
+  cart_id INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(50) NOT NULL,
+  item_name VARCHAR(50) NOT NULL)
+  ENGINE = InnoDB;
 INSERT INTO users VALUES
   ('buyer', '123', 'buyer'),
   ('admin', '123', 'seller');
