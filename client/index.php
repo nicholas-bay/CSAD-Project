48eff084ddf155b697943efa836bdb5b33674b97
Nicholas Bay <70332449+Nicholas-Bay@users.noreply.github.com>
2022-02-13 15:39:39 +0800

    update

--- client/index.php
+++ client/index.php
@@ -1,7 +1,7 @@
 <?php 
   session_start();
   $_SESSION['username'] = 'User';
-  $_SESSION['searchpointer'] = NULL;
+  $_SESSION['pointer'] = NULL;
   $_SESSION['product_count'] = array();
   $_SESSION['login_valid'] = "";
   $_SESSION['userstate'] = NULL;
